6eabfdfd0a66779603af20016b60cc751917f537
BoySanic <boysonicrevived@gmail.com>
2025-01-11 01:20:36 -0800

    feat(loneseed): Add loneseed project files and configuration

---
+++ images/makeproject/project/loneliest-workgen.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#command_line has the --device flag to trick the wrapper into passing args to the child app
+for i in {0..262143}; do
+wu_name="loneseed_1.00_$i"
+  echo "create_work: ${wu_name}"
+  bin/create_work --appname loneseed \
+    --wu_template templates/loneseed_in \
+    --result_template templates/loneseed_out \
+    --command_line "--start $((i * 1024)) --end $(((i + 1) * 1024))\"" \
+    --wu_name "${wu_name}" \
+    --min_quorum 2 \
+    --credit 2500
+
+done
