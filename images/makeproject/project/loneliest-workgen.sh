7a80b76156e50b9a3c84e61cffaebbc069043778
BoySanic <boysonicrevived@gmail.com>
2025-01-11 16:03:51 -0800

    fix(loneseed): extraneous quote in commandline args in workgen

--- images/makeproject/project/loneliest-workgen.sh
+++ images/makeproject/project/loneliest-workgen.sh
@@ -6,7 +6,7 @@ wu_name="loneseed_1.00_$i"
   bin/create_work --appname loneseed \
     --wu_template templates/loneseed_in \
     --result_template templates/loneseed_out \
-    --command_line "--start $((i * 1024)) --end $(((i + 1) * 1024))\"" \
+    --command_line "--start $((i * 1024)) --end $(((i + 1) * 1024))" \
     --wu_name "${wu_name}" \
     --min_quorum 2 \
     --credit 2500
