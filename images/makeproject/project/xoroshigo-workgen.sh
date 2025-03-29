f74b169f2c61ad10dcefccd8a60bfd4f21b80124
BoySanic <boysonicrevived@gmail.com>
2025-03-29 00:55:16 -0700

    fix(xoroshigo): 1.04 - hopefully last fix for tempdir and numpy import?

--- images/makeproject/project/xoroshigo-workgen.sh
+++ images/makeproject/project/xoroshigo-workgen.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 for i in {0..10000}; do
-    wu_name="xoroshigo_1.00_config-001-hixorlo-fullinfo-rank100_$i"
+    wu_name="xoroshigo_1.04_config-001-hixorlo-fullinfo-rank100_$i"
     echo "create_work: ${wu_name}"
     bin/create_work --appname xoroshigo \
         --wu_template templates/xoroshigo_in \
         --result_template templates/xoroshigo_out \
-        --command_line "config-001-hixorlo-fullinfo-rank100.npz 1500000 $(i) input.npz" \
+        --command_line "config-001-hixorlo-fullinfo-rank100.npz 1500000 $i input.npz" \
         --wu_name "${wu_name}" \
         --min_quorum 2 \
         --credit 2500 \
-    xoroshigo_configs/config-001-hixorlo-fullinfo-rank100.npz 
+    config-001-hixorlo-fullinfo-rank100.npz 
 
 done
