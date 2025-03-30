70f463b129d33d74d7d4acef0575ca5b24006506
BoySanic <boysonicrevived@gmail.com>
2025-03-30 02:38:04 -0700

    fix(xoroshiro): Fix workgen script to use correct config

--- images/makeproject/project/xoroshigo-workgen.sh
+++ images/makeproject/project/xoroshigo-workgen.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for i in {0..10000}; do
+for i in {9999..20000}; do
     wu_name="xoroshigo_1.04_config-001-hixorlo-fullinfo-rank100_$i"
     echo "create_work: ${wu_name}"
     bin/create_work --appname xoroshigo \
         --wu_template templates/xoroshigo_in \
         --result_template templates/xoroshigo_out \
-        --command_line "--device \"config-001-hixorlo-fullinfo-rank100.npz 1500000 $i input.npz\"" \
+        --command_line "--device \"config-001-hixorlo-fullinfo-rank100-genfix.npz 1500000 $i input.npz\"" \
         --wu_name "${wu_name}" \
         --min_quorum 2 \
         --credit 5000
