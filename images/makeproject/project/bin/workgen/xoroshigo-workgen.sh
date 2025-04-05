54f2b7eb373f21455d7404b65733b56622092e21
BoySanic <boysonicrevived@gmail.com>
2025-04-05 14:13:27 -0700

    feat(xoroshigo2): Add more extensible workgen script

--- images/makeproject/project/bin/workgen/xoroshigo-workgen.sh
+++ images/makeproject/project/bin/workgen/xoroshigo-workgen.sh
@@ -1,73 +1,25 @@
 #!/bin/bash
+WU_VERSION="2.06"
+declare -i LOWER_BOUND=0
+declare -i UPPER_BOUND=9999
 
 for file in ./xoroshigo_configs/*;
 do
+    STRIPPED_FILENAME=$(basename $file .npz)
+    FILENAME=$(basename $file)
     echo "Staging file $file"
     bin/stage_file --verbose --copy "$file"
-done
+    #Conf2
+    for i in $(seq $LOWER_BOUND $UPPER_BOUND); do
+        wu_name="xoroshigo_${WU_VERSION}_${STRIPPED_FILENAME}_${i}"
+        echo "create_work: ${wu_name}"
+        bin/create_work --appname xoroshigo2 \
+            --wu_template templates/xoroshigo_in_$STRIPPED_FILENAME \
+            --result_template templates/xoroshigo_out \
+            --command_line "--passthrough_child $filename 30000000 $i input.npz" \
+            --wu_name "${wu_name}" \
+            --min_quorum 2 \
+            --credit 5000
 
-#Conf2
-for i in {0..9999}; do
-    wu_name="xoroshigo_2.05_config-002-regular-fullinfo-rank100_$i"
-    echo "create_work: ${wu_name}"
-    bin/create_work --appname xoroshigo2 \
-        --wu_template templates/xoroshigo_in_config-002-regular-fullinfo-rank100 \
-        --result_template templates/xoroshigo_out \
-        --command_line "--passthrough_child config-002-regular-fullinfo-rank100.npz 30000000 $i input.npz" \
-        --wu_name "${wu_name}" \
-        --min_quorum 2 \
-        --credit 5000
-
-done
-#Conf3
-for i in {0..9999}; do
-    wu_name="xoroshigo_2.05_config-003-hxlreg-fullinfo-rank100_$i"
-    echo "create_work: ${wu_name}"
-    bin/create_work --appname xoroshigo2 \
-        --wu_template templates/xoroshigo_in_config-003-hxlreg-fullinfo-rank100 \
-        --result_template templates/xoroshigo_out \
-        --command_line "--passthrough_child config-003-hxlreg-fullinfo-rank100.npz 30000000 $i input.npz" \
-        --wu_name "${wu_name}" \
-        --min_quorum 2 \
-        --credit 5000
-
-done
-#Conf4
-for i in {0..9999}; do
-    wu_name="xoroshigo_2.05_config-004-hixorlo-fullinfo-rank080_$i"
-    echo "create_work: ${wu_name}"
-    bin/create_work --appname xoroshigo2 \
-        --wu_template templates/xoroshigo_in_config-004-hixorlo-fullinfo-rank080 \
-        --result_template templates/xoroshigo_out \
-        --command_line "--passthrough_child config-004-hixorlo-fullinfo-rank080.npz 30000000 $i input.npz" \
-        --wu_name "${wu_name}" \
-        --min_quorum 2 \
-        --credit 5000
-
-done
-#Conf5
-for i in {0..9999}; do
-    wu_name="xoroshigo_2.05_config-005-regular-fullinfo-rank080_$i"
-    echo "create_work: ${wu_name}"
-    bin/create_work --appname xoroshigo2 \
-        --wu_template templates/xoroshigo_in_config-005-regular-fullinfo-rank080 \
-        --result_template templates/xoroshigo_out \
-        --command_line "--passthrough_child config-005-regular-fullinfo-rank080.npz 30000000 $i input.npz" \
-        --wu_name "${wu_name}" \
-        --min_quorum 2 \
-        --credit 5000
-
-done
-#Conf6
-for i in {0..9999}; do
-    wu_name="xoroshigo_2.05_config-006-hxlreg-fullinfo-rank080_$i"
-    echo "create_work: ${wu_name}"
-    bin/create_work --appname xoroshigo2 \
-        --wu_template templates/xoroshigo_in_config-006-hxlreg-fullinfo-rank080 \
-        --result_template templates/xoroshigo_out \
-        --command_line "--passthrough_child config-005-regular-fullinfo-rank080.npz 30000000 $i input.npz" \
-        --wu_name "${wu_name}" \
-        --min_quorum 2 \
-        --credit 5000
-
+    done
 done
