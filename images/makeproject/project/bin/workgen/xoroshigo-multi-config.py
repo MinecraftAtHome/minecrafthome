92642c27e5006d1bb2169ec054f95df2730ea379
BoySanic <boysonicrevived@gmail.com>
2025-04-23 15:48:05 -0700

    feat(xoroshigo2): Add macos binaries, general fixes for panopale template and xoroshigo2 workgen script

--- images/makeproject/project/bin/workgen/xoroshigo-multi-config.py
+++ images/makeproject/project/bin/workgen/xoroshigo-multi-config.py
@@ -5,21 +5,30 @@ import subprocess
 import argparse
 import os
 
-WU_VERSION="2.07"
-LOWER_BOUND=0
-UPPER_BOUND=9999
-CONFIG_RANGE="0-100000"
+CONFIG_RANGE = "0-100000"
+CONFIG_DIR = "./xoroshigo_config"
+WU_VERSION = "2.09"
+LOWER_BOUND = 0
+UPPER_BOUND = 9999
+BATCH = 0
 parser = argparse.ArgumentParser(description="Creatework for xoroshigo2 - single config")
-parser.add_argument("-l", "--lowerbound", type=int, help="Lower bound value")
-parser.add_argument("-u", "--upperbound", type=int, help="Upper bound value")
+parser.add_argument("-l", "--lowerbound", type=int, default=0, help="Lower bound value")
+parser.add_argument("-u", "--upperbound", type=int, default=9999, help="Upper bound value")
 parser.add_argument("-c", "--config_dir", help="Path to config file")
-parser.add_argument("-v", "--wu_version", help="WU Version for wu_name concat")
-parser.add_argument("-r", "--range", help="Integer range of config files. syntax: lo-hi (aka 0-6, 0-128, etc)")
+parser.add_argument("-v", "--wu_version", default="2.09", help="WU Version for wu_name concat")
+parser.add_argument("-r", "--range", default="0-100000", help="Integer range of config files. syntax: lo-hi (aka 0-6, 0-128, etc)")
+parser.add_argument("-b", "--batch", default=0, help="Batch number for boinc database, optional.")
 args = parser.parse_args()
-LOWER_BOUND = args.lowerbound
-UPPER_BOUND = args.upperbound
-CONFIG_DIR = args.config_dir
-CONFIG_RANGE = args.range
+if(args.wu_version ):
+    WU_VERSION = args.wu_version
+if(args.lowerbound != LOWER_BOUND):
+    LOWER_BOUND = args.lowerbound
+if(args.upperbound != UPPER_BOUND):
+    UPPER_BOUND = args.upperbound
+if(args.config_dir != CONFIG_DIR):
+    CONFIG_DIR = args.config_dir
+if(args.config_range != CONFIG_RANGE):
+    CONFIG_RANGE = args.config_range
 CONFIG_LO = int(CONFIG_RANGE.split("-")[0])
 CONFIG_HI = int(CONFIG_RANGE.split("-")[1])
 
