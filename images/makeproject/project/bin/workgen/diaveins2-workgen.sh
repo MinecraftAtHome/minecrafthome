f04dcedbdf164ead59f91b5c70d53103cbbf6747
ScriptLine <85095943+ScriptLineStudios@users.noreply.github.com>
2026-07-25 00:18:44 +0200

    Diaveins phase2 (#89)
    * diaveins2
    
    * forgot the assimilator
    
    * increase crdit per task
    
    * update fpops

---
+++ images/makeproject/project/bin/workgen/diaveins2-workgen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for i in {0..65536}; do
+wu_name="diaveins2_1.00_$i"
+  echo "create_work: ${wu_name}"
+  bin/create_work --appname diaveins2 \
+    --wu_template templates/diaveins2_in \
+    --result_template templates/diaveins2_out \
+    --command_line "--start $((i * 4096)) --end $(((i + 1) * 4096))" \
+    --wu_name "${wu_name}" \
+    --min_quorum 2 \
+    --credit 34000
+
+done
